e31f517594f594525230b537865355e6f2756f9c
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2022-09-03 21:50:09 -0700

    Chess AI Board Created

---
+++ Python Projects/Chess AI/Chess.py
@@ -0,0 +1,56 @@
+import pygame
+import ChessEngine
+
+
+pygame.init()
+WIDTH = HEIGHT = 512
+DIMENSIONS = 8
+SQ_SIZE = HEIGHT // DIMENSIONS
+MAX_FPS = 15
+IMAGES = {}
+
+def loadImages():
+    peices = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for peice in peices:
+        IMAGES[peice] = pygame.transform.scale(pygame.image.load(peice + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+    SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    SCREEN.fill(pygame.Color("white"))
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        drawGameState(SCREEN, gs)
+        clock.tick(MAX_FPS)
+        pygame.display.flip()
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPeices(screen, gs.board)
+    
+
+
+def drawBoard(screen):
+    colors = [pygame.Color("white"), pygame.Color("grey")]
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            color = colors[((r+c)%2)]
+            pygame.draw.rect(screen, color, pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPeices(screen, board):
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            peice = board[c][r]
+            if peice != "--":
+                screen.blit(IMAGES[peice], pygame.Rect(r*SQ_SIZE, c*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python Projects/Chess AI/ChessEngine.py
@@ -0,0 +1,17 @@
+class GameState():
+    def __init__(self):
+        #First letter represents the color, b for black, and w for white
+        #The second letter represents the peice
+        #"--" represents a blank space
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+            ]
+        self.whiteToMove = True
+        self.moveLog = []

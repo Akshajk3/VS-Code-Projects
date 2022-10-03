d42f624a4a7581ca2af9e10162f382c64e49bfd2
Akshaj <akshaj.kanumuri@gmail.com>
2022-10-02 20:18:30 -0700

    all packages installed

--- Web Development/Java Script Projects/Minecraft Test/my-app/src/App.js
+++ Web Development/Java Script Projects/Minecraft Test/my-app/src/App.js
@@ -1,10 +1,12 @@
 import { Canvas } from '@react-three/fiber';
+import {Sky} from '@react-three/drei';
 
 function App() {
   return (
     <>
       <div>Outside Canvas</div>
       <Canvas>
+        <Sky />
       </Canvas>
     </>
   );

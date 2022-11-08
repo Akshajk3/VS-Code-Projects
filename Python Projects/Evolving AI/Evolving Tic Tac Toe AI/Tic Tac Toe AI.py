b03bcb215d107ab544720900bef1c24d6a7ab089
Akshaj <akshaj.kanumuri@gmail.com>
2022-11-07 19:21:48 -0800

    Update Tic Tac Toe AI.py

--- Python Projects/Evolving AI/Evolving Tic Tac Toe AI/Tic Tac Toe AI.py
+++ Python Projects/Evolving AI/Evolving Tic Tac Toe AI/Tic Tac Toe AI.py
@@ -41,77 +41,32 @@ class Toc:
 
     def draw(self):
         SCREEN.blit(self.img, (self.X_POS, self.Y_POS))
-        
 
 class Tile:
+    def __init__(self, x_pos, y_pos):
+        self.rect = pygame.draw.rect()
 
-    def __init__(self):
-        self.positions = [(90, 90), (215, 90), (340, 90)]
-        i = 0
-        while i < 3:
-            TILES[i] = pygame.draw.rect(SCREEN, COLORS, (POS_X[i], POS_Y[i], 30, 30))
-            i += 1
-
-
-    def draw(self, COLOR, draw):
-        i = 0
-        if DRAW == False:
-            while i < 3:
-                tileDraw[i] = True
-                pygame.draw.rect(SCREEN, COLOR, (POS_X[i], POS_Y[i], 30, 30))
-                for event in pygame.event.get():
-                    if pygame.rect.collidepoint(pygame.mouse.get_pos()):
-                        COLOR = RED
-                        if event.type == pygame.MOUSEBUTTONDOWN:
-                            draw = True
-                    else:
-                        COLOR = GREEN
-                i += 1
-        
-
-class X:
-    
-    def __init__(self, img=X_IMG):
-        self.img = img
-        self.X_POS = 0
-        self.Y_POS = 0
-    
-    def draw(self):
-        SCREEN.blit(self.img, (30, 50))
 
 def update_ui(COLOR):
     board = Toc()
-    tile = Tile()
-    XP = X()
-    SCREEN.fill((255,255,255))
+    SCREEN.fill((255, 255, 255))
+    
     board.draw()
-    tile.draw(COLORS, DRAW)
-    if DRAW == True:
-        XP.draw()
+    
 
 def main():
-    global positions, tileDraw
-
-    clock = pygame.time.Clock()
-
-    tileDraw = [False, False, False]
-
-    player = Toc()
-    X_P = X()
-    tile = Tile()
-
     run = True
-    
-    while run:
+    toc = Toc()
 
+    while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        
-        update_ui(COLORS)
-        clock.tick(30)
-        pygame.display.update()
+            update_ui(RED)
+            pygame.display.update()
 
-main()
+
+if __name__ == "__main__":
+    main()

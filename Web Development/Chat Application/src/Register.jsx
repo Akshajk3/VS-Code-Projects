f35bf2b1fa5f1d1c01b6c1aaa2b60c7ef781db23
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2023-03-01 21:47:59 -0800

    fixed some minor bugs

--- Web Development/Chat Application/src/Register.jsx
+++ Web Development/Chat Application/src/Register.jsx
@@ -12,49 +12,49 @@ const Register = () => {
 
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        const displayName = e.target[0].value;
-        const email = e.target[1].value;
-        const password = e.target[2].value;
-        const file = e.target[3].files[0];
-
-        try{
-            const res = await createUserWithEmailAndPassword(auth, email, password);
-
-            const storageRef = ref(storage, displayName);
-
-            const uploadTask = uploadBytesResumable(storageRef, file);
-
-            // Register three observers:
-            uploadTask.on(
-                (error) => {
-                    setErr(true);
-                }, 
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-                        await updateProfile(res.user, {
-                            displayName,
-                            photoURL: downloadURL,
-                          });
-
-                          await setDoc(doc(db, "users", res.user.uid), {
-                            uid: res.user.uid,
-                            displayName,
-                            email,
-                            photoURL: downloadURL,
-                          });
-
-                          await setDoc(doc(db, "userChats", res.user.uid), {});
-                          navigate("/");
-
-                    });
-                }
-            );
-        }catch(err){
-            setErr(true);
-        }
+      e.preventDefault();
+      const displayName = e.target[0].value;
+      const email = e.target[1].value;
+      const password = e.target[2].value;
+      const file = e.target[3].files[0];
+  
+      try {
+        //Create user
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+  
+        //Create a unique image name
+        const date = new Date().getTime();
+        const storageRef = ref(storage, `${displayName + date}`);
+  
+        await uploadBytesResumable(storageRef, file).then(() => {
+          getDownloadURL(storageRef).then(async (downloadURL) => {
+            try {
+              //Update profile
+              await updateProfile(res.user, {
+                displayName,
+                photoURL: downloadURL,
+              });
+              //create user on firestore
+              await setDoc(doc(db, "users", res.user.uid), {
+                uid: res.user.uid,
+                displayName,
+                email,
+                photoURL: downloadURL,
+              });
+  
+              //create empty user chats on firestore
+              await setDoc(doc(db, "userChats", res.user.uid), {});
+              navigate("/");
+            } catch (err) {
+              console.log(err);
+              setErr(true);
+            }
+          });
+        });
+      } catch (err) {
+        setErr(true);
+      }
     };
-
     return(
         <div className="formContainer">
 
@@ -64,14 +64,14 @@ const Register = () => {
                 <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Display name"/>
                     <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
+                    <input type="password" placeholder="Password (At least 6 characters)"/>
                     <input style={{display: "none"}} type="file" id="file"/>
                     <label htmlFor="file">
                         <img src={icon} alt="" />
                         <span>Add an avatar</span>
                     </label>
                     <button>Sign Up</button>
-                    {err && <span>Something went wrong</span>}
+                    {err && <span>Something went wrong {err}</span>}
                 </form>
                 <p>You do have an account? <Link to="/login">Login</Link></p>
             </div>

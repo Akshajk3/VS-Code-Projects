3ef191c7a53cd4220b5974655183f8c15cc60d2d
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2023-03-26 23:45:30 -0700

    update

--- Python Projects/Coding Challenges/String_Parser.py
+++ Python Projects/Coding Challenges/String_Parser.py
@@ -3,24 +3,29 @@ def parseString(message):
     last_name = ""
     id = ""
 
+    message2 = ""
+
+    for letter in message:
+        if letter != "0":
+            message2 += letter
+    
     i = 0
 
-    while i < 2:
-        for letter in message:
+    while i < 3:
 
-            if letter == "0":
-                i += 1
-                
-            if i == 0:
-                first_name += letter
-            elif i == 1:
-                last_name += letter
-            elif i == 2:
-                id += letter
+        if letter == letter.Upper():
+            i += 1
+        
+        if i == 0:
+            first_name += letter
+        elif i == 1:
+            last_name += letter
+        elif i == 2:
+            id += letter
 
     string = {"first_name": first_name , "last_name": last_name , "id": id }
     
-    return string
+    return string["last_name"]
     
 
 

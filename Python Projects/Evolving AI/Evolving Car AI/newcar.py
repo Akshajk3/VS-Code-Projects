15e97468ee3b168c1f320ec4526aeced138cf8c4
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2023-04-09 14:40:01 -0700

    Update newcar.py

--- Python Projects/Evolving AI/Evolving Car AI/newcar.py
+++ Python Projects/Evolving AI/Evolving Car AI/newcar.py
@@ -6,8 +6,8 @@ import os
 import neat
 import pygame
 
-WIDTH = 1600
-HEIGHT = 880
+WIDTH = 1920
+HEIGHT = 1080
 
 CAR_SIZE_X = 60
 CAR_SIZE_Y = 60
@@ -15,6 +15,10 @@ CAR_SIZE_Y = 60
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+BORDER_COLOR = (255, 255, 255)
+
+current_generation = 0
 
 class Car:
     def __init__(self):
@@ -46,6 +50,169 @@ class Car:
             position = radar[0]
             pygame.draw.line(screen, GREEN, self.center, position)
             pygame.draw.circle(screen, RED, position, 5)
+        
+    def check_collision(self, game_map):
+        self.alive = True
+        for point in self.corners:
+            if game_map.get_at((int(point[0]), int(point[1]))) == BORDER_COLOR:
+                self.alive = False
+                break
+
+    def check_radar(self, degree, game_map):
+        length = 0
+        x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * length)
+        y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * length)
+
+        while not game_map.get_at((x, y)) == BORDER_COLOR and length < 300:
+            length = length + 1
+            x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * length)
+            y = int(self.center[1] + math.cos(math.radians(360 - (self.angle + degree))) * length)
+
+        dist = int(math.sqrt(math.pow(x - self.center[0], 2) + math.pow(y - self.center[1], 2)))
+        self.radars.append([(x, y), dist])
     
     def update(self, game_map):
-        pass
+        if not self.speed_set:
+            self.speed = 20
+            self.speed_set = True
+
+        self.rotated_sprite = self.rotate_center(self.sprite, self.angle)
+        self.position[0] += math.cos(math.radians(360 - self.angle)) * self.speed
+        self.position[0] = max(self.position[0], 20)
+        self.position[0] = min(self.position[0], WIDTH - 120)
+
+        self.distance += self.speed
+        self.time += 1
+        
+        self.position[1] += math.sin(math.radians(360 - self.angle)) * self.speed
+        self.position[1] = max(self.position[1], 20)
+        self.position[1] = min(self.position[1], WIDTH - 120)
+
+        self.center = [int(self.position[0]) + CAR_SIZE_X / 2, int(self.position[1]) + CAR_SIZE_Y / 2]
+
+        length = 0.5 * CAR_SIZE_X
+        left_top = [self.center[0] + math.cos(math.radians(360 - (self.angle + 30))) * length, self.center[1] + math.sin(math.radians(360 - (self.angle + 30))) * length]
+        right_top = [self.center[0] + math.cos(math.radians(360 - (self.angle + 150))) * length, self.center[1] + math.sin(math.radians(360 - (self.angle + 150))) * length]
+        left_bottom = [self.center[0] + math.cos(math.radians(360 - (self.angle + 210))) * length, self.center[1] + math.sin(math.radians(360 - (self.angle + 210))) * length]
+        right_bottom = [self.center[0] + math.cos(math.radians(360 - (self.angle + 330))) * length, self.center[1] + math.sin(math.radians(360 - (self.angle + 330))) * length]
+        self.corners = [left_top, right_top, left_bottom, right_bottom]
+
+        self.check_collision(game_map)
+        self.radars.clear()
+
+        for d in range(-90, 120, 45):
+            self.check_radar(d, game_map)
+
+
+    def get_data(self):
+        radars = self.radars
+        return_values = [0, 0, 0, 0, 0]
+        for i, radar in enumerate(radars):
+            return_values[i] = int(radar[1] / 30)
+        
+        return return_values
+
+    def is_alive(self):
+        return self.alive
+
+    def get_reward(self):
+        return self.distance / (CAR_SIZE_X / 2)
+
+    def rotate_center(self, image, angle):
+        rectangle = image.get_rect()
+        rotated_image = pygame.transform.rotate(image, angle)
+        rotated_rectangle = rectangle.copy()
+        rotated_rectangle.center = rotated_image.get_rect().center
+        rotated_image = rotated_image.subsurface(rotated_rectangle).copy()
+        return rotated_image
+    
+def run_simulation(genomes, config):
+    nets = []
+    cars = []
+
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    for i, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        g.fitness = 0
+
+        cars.append(Car())
+    
+    clock = pygame.time.Clock()
+    generation_font = pygame.font.SysFont("Arial", 30)
+    alive_font = pygame.font.SysFont("Arial", 20)
+    game_map = pygame.image.load('map.png').convert()
+
+    global current_generation
+    current_generation += 1
+
+    counter = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+        
+        for i, car in enumerate(cars):
+            output = nets[i].activate(car.get_data())
+            choice = output.index(max(output))
+            if choice == 0:
+                car.angle += 10
+            elif choice == 1:
+                car.angle -= 10
+            elif choice == 2:
+                if(car.speed - 2 >= 12):
+                    car.speed -= 2
+            else:
+                car.speed += 2
+
+        still_alive = 0
+
+        for i, car in enumerate(cars):
+            if car.is_alive():
+                still_alive += 1
+                car.update(game_map)
+                genomes[i][1].fitness += car.get_reward()
+
+        
+        if still_alive == 0:
+            break
+
+        counter += 1
+        if counter == 30 * 40:
+            break
+
+        screen.blit(game_map, (0, 0))
+        for car in cars:
+            if car.is_alive:
+                car.draw(screen)
+        
+        text = generation_font.render("Generation: " + str(current_generation), True, (0, 0, 0))
+        text_rect = text.get_rect()
+        text_rect.center = (900, 450)
+        screen.blit(text, text_rect)
+
+        text = alive_font.render("Still Alive: " + str(still_alive), True, (0 ,0, 0))
+        text_rect = text.get_rect()
+        text_rect.center = (900, 490)
+        screen.blit(text, text_rect)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    config_path = "./config.txt"
+    config = neat.config.Config(neat.DefaultGenome,
+                                neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation,
+                                config_path)
+    population = neat.Population(config)
+    population.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    population.add_reporter(stats)
+
+    population.run(run_simulation, 1000) 

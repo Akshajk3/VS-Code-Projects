1b0d8c02d93038d2096ff65d97b3798983a3410a
Akshaj <akshaj.kanumuri@gmail.com>
2023-09-09 11:17:36 -0700

    update

--- C++ Projects/SDL2/Vindleview Valley/Vindleview Valley/Player.h
+++ C++ Projects/SDL2/Vindleview Valley/Vindleview Valley/Player.h
@@ -10,4 +10,5 @@ public:
 	Player(float p_x, float p_y, SDL_Renderer* ren, Animation anim, std::map<std::string, std::vector<SDL_Texture*>> asset);
 
 	void update(int movement[2]);
+	void attack();
 };

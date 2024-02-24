fb48eca5cb89ca76c0ed9a177b998a99e202f702
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2024-02-23 22:35:08 -0800

    update

--- ASM/Z80/test.asm
+++ ASM/Z80/test.asm
@@ -1,8 +1,51 @@
-org 0x100
+; Z80 Assembly Equivalent Code
 
-start:
-    ld b, 5
-    inc b
-    halt
+; Load accumulator with value 0xFF
+LD A, 0xFF
 
-    end
+; Store accumulator value at memory address 0x0002
+LD (0x0002), A
+
+; Return from subroutine
+RET
+
+; Load register B with value 0x00
+LD B, 0x00
+
+; Load accumulator with value at memory address 0xC01A
+LD A, (0xC01A)
+
+; Store accumulator value at memory address 0x0000
+LD (0x0000), A
+
+; Return from subroutine
+RET
+
+; Increment value in register B by one
+INC B
+
+; Call subroutine at memory address 0xC018
+CALL 0xC018
+
+; Compare value at memory address 0x0AE0 with register B
+LD A, (0x0AE0)
+CP B
+JR NZ, loop  ; Jump to loop if not equal
+
+; Unconditional branch to memory address 0xC007
+JP 0xC007
+
+; No operation
+NOP
+
+; Return from subroutine
+RET
+
+; Compare accumulator with value at memory address 0xF9
+LD A, (0xF9)
+CP A
+
+; Various other instructions depending on the specific operations and data
+
+; Define a loop label
+loop:
--- Arduino/Z80/Z80_Controller/Z80_Controller.ino
+++ Arduino/Z80/Z80_Controller/Z80_Controller.ino
@@ -9,6 +9,14 @@ const char DATA[] = {22, 24, 26, 28, 30, 32, 34, 36};
 #define CLK_PIN 7
 #define RESET_PIN 8
 
+byte ram[1024];
+const unsigned int RAM_START = 0x0000000;
+
+byte rom[] = { 0xa9, 0xff, 0x8d, 0x02, 0x60, 0xa2, 0x00, 0xbd, 0x1a, 0xc0, 0x8d, 0x00, 0x60, 0xe8, 0x20, 0x18, 0xc0, 0xe0, 0x0a, 0xf0, 0xf0, 0x4c, 0x07, 0xc0, 0xea, 0x60, 0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 
+                        0x02, 0xf8, 0x80, 0x90};
+const unsigned int ROM_START = 0x8000;
+
+
 void setup() {
   // put your setup code here, to run once:
   for (int i = 0; i < 16; i++)
@@ -32,14 +40,52 @@ void onClock()
   unsigned int data = 0;
 
 
-  Serial.print("ADDR: ")
+  Serial.print("ADDR: ");
   for (int i = 0; i < 16; i++)
   {
+    int bit = digitalRead(ADDR[i]) ? 1 : 0;
+    Serial.print(bit);
+    data = (data << 1) + bit;
+  }
 
+  Serial.print("    DATA: ");
+
+  for (int i = 0; i < 8; i++)
+  {
+    int bit = digitalRead(DATA[i]) ? 1 : 0;
+    Serial.print(bit);
+    data = (data << 1) + bit;
+  }
+
+  sprintf(output, "   %04x  %c  %02x", addr, digitalRead(WR) ? 'r' : 'w, data');
+  Serial.println(output);
+}
+
+void pulseClock()
+{
+  digitalWrite(CLK_PIN, HIGH);
+  delay(500);
+  digitalWrite(CLK_PIN, LOW);
+  delay(500);
+}
+
+void write(unsigned int data)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    pinMode(DATA[i], OUTPUT);
+  }
+
+  unsigned int mask = 0b10000000;
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(DATA[i], data & mask ? 1 : 0);
+    mask = mask >> 1;
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  onClock();
+  pulseClock();
 }

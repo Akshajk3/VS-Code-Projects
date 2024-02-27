5fe438e76ea9cf735b2ea026a6c96dceec31a571
Akshaj <akshaj.kanumuri@gmail.com>
2024-02-27 01:49:18 -0800

    update

--- Arduino/Z80/Clock/Clock.ino
+++ Arduino/Z80/Clock/Clock.ino
@@ -8,7 +8,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(CLK_PIN, HIGH);
-  delay(500); 
+  delay(100); 
   digitalWrite(CLK_PIN, LOW);
-  delay(500);
+  delay(100);
 }
